ff3cc5c47bb46a15b94e97fcc80aee36be50f067
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-24 20:31:47 +0200

    Remove focus from search button after bringing up modal
    This is done as prevent this modal from reopening when using the enter
    to search due to button being clicked by using enter.

--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -11,7 +11,7 @@
 <aside id="navbar">
 	<div id="logo-space"><img alt="OSINTer logo" src="https://logos-download.com/wp-content/uploads/2019/07/Feedly_Logo-700x231.png"/></div>
 
-	<button class="long-button" on:click={() => $modalState.modalType = "search"}> <Icon name="magnifying-glass"/> <span> Explore Content </span> </button>
+	<button class="long-button" on:click={() => { $modalState.modalType = "search"; document.activeElement.blur(); } }> <Icon name="magnifying-glass"/> <span> Explore Content </span> </button>
 
 	<nav>
 		<Menu menuOptions={mainFeeds} menuType="feed" />
