f1f17c10f4d83e2567ae175c7905416a73264700
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-24 19:17:45 +0200

    Add rest of input fields and search button

--- src/components/modals/search/main.svelte
+++ src/components/modals/search/main.svelte
@@ -1,10 +1,12 @@
 <script>
 	import SourceSelect from "./sourceSelect.svelte"
 	import Modal from "../modal.svelte"
+	import Params from "./general.svelte"
 
 	import { writable } from 'svelte/store'
 	
 	import { appConfig } from "../../../shared/config.js"
+	import { search } from "../../../lib/search.js"
 
 	const searchSpecs = writable({
 		"sourceCategory" : [],
@@ -17,6 +19,35 @@
 		"highlight" : false
 	})
 
+	const inputFields = {
+		"timespan" : [
+			{ "readable" : "First Date", "name" : "firstDate" },
+			{ "readable" : "Last Date", "name" : "lastDate" }
+		],
+		"sorting" : [
+			{
+				"readable" : "Sort Order",
+				"name" : "sortOrder",
+				"options" : [
+					[ "desc", "Descending"],
+					[ "asc", "Ascending"]
+				]
+			},
+			{
+				"readable" : "Sort By",
+				"name" : "sortBy",
+				"options" : [
+					["publish_date", "Publish Date"],
+					["read_times", "Times read"],
+					["source", "Source"],
+					["author", "Author"],
+					["url", "URL"],
+					["inserted_at", "Time of scraping"]
+				]
+			}
+		]
+	}
+
 </script>
 
 
@@ -25,6 +56,40 @@
 		<div class="optionSelectContainer">
 			<SourceSelect searchSpecs={searchSpecs}/>
 		</div>
+
+		<Params inputTitle="Timespan" inputDesc="Enter wanted date interval. Only articles published within this interval will be shown.">
+			{#each inputFields.timespan as inputDesc}
+				<div class="inputContainer">
+					<label for="{inputDesc.name}">{inputDesc.readable}</label>
+					<input bind:value={$searchSpecs[inputDesc.name]} id="{inputDesc.name}" name="{inputDesc.name}" type="date">
+				</div>
+			{/each}
+		</Params>
+
+		<Params inputTitle="Sorting" inputDesc="Choose how to sort articles and whether they should be sorted ascending or descending.">
+			{#each inputFields.sorting as inputDesc}
+				<div class="inputContainer">
+					<label for="{inputDesc.name}">{inputDesc.readable}</label>
+					<select bind:value={$searchSpecs[inputDesc.name]} id="{inputDesc.name}" name="{inputDesc.name}">
+						{#each inputDesc.options as [ value, name ] }
+							<option value="{value}">{name}</option>
+						{/each}
+					</select>
+				</div>
+			{/each}
+		</Params>
+
+		<Params inputTitle="Limit" inputDesc="Limit number of articles queried from server. Higher number will lead to increased server-load.">
+			<label for="limit-input">Limit</label>
+			<input bind:value={$searchSpecs["limit"]} id="limit-input" name="limit-input" type="number" min="1" max="1000">
+		</Params>
+
+		<Params inputTitle="Search Term" inputDesc="Uses nearly same syntax as Google Dorks. Enable highlighting search matches.">
+			<input bind:value={$searchSpecs["searchTerm"]} id="searchTerm" name="searchTerm" type="text" placeholder="Search Term">
+		</Params>
+
+		<button on:click={() => search($searchSpecs)}>Search content</button>
+
 	</div>
 </Modal>
 
@@ -33,8 +98,7 @@ div.contentContainer {
 	display: grid;
 
 	grid-template-columns: repeat(2, 1fr);
-	grid-template-rows: repeat(4, 1fr);
-	gap: 2rem 2rem;
+	grid-template-rows: repeat(4, 3fr) 2fr;
 
 	padding: 2rem;
 	box-sizing: border-box;
@@ -50,5 +114,34 @@ div.optionSelectContainer {
 
 	display: flex;
 	flex-direction: column;
+
+	border-right: 1px solid #efefef;
+
+	border-bottom: 1px solid #efefef;
+
+	padding-right: 2rem;
+}
+
+button {
+
+	@include button;
+
+	align-self: center;
+	justify-self: center;
+
+	width: 60%;
+	height: 5rem;
+
+	padding: 1rem;
+
+	border-radius: 2px;
+	border: 1px solid rgb(90, 90, 90, 0.2);
+
+
+	@include font(0.5, 700);
+	text-transform: uppercase;
+
+	grid-column-start: 1;
+	grid-column-end: 3;
 }
 </style>
