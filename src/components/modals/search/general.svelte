df447644280aa66e49841451ba5247071df8c89e
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-25 12:22:57 +0200

    Add initial mockup of general input options in search modal

---
+++ src/components/modals/search/general.svelte
@@ -0,0 +1,47 @@
+<script>
+	export let inputTitle
+	export let inputDesc
+</script>
+
+<div class="paramContainer">
+	<h2>{ inputTitle }</h2>
+	<p> - { inputDesc }</p>
+	<hr>
+
+	<form on:submit|preventDefault>
+		<slot></slot>
+	</form>
+</div>
+
+<style type="text/scss">
+div.paramContainer {
+	padding-left: 2rem;
+
+	h2, p {
+		display: inline;
+	}
+
+	h2 {
+		@include font(0.8, 700, 1.5rem);
+	}
+
+	p {
+		@include font(0.8, 200, 0.8rem);
+	}
+
+	hr {
+		margin-bottom: 1rem;
+	}
+
+	form {
+		display: flex;
+
+
+		:global(div.inputContainer) {
+			display: flex;
+			flex-direction: column;
+		}
+		
+	}
+}
+</style>
