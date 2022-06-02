b5f3910e8beebf774a749476946e45cad08633c9
bertmad3400 <skrivtilbertram@gmail.com>
2022-06-02 17:04:04 +0200

    Allow for specifying height and width of modal

--- src/components/modals/modal.svelte
+++ src/components/modals/modal.svelte
@@ -2,10 +2,12 @@
 	import { modalState } from "../../shared/stores.js"
 	import { fly, fade } from 'svelte/transition';
 
+	export let width = "80vw"
+	export let height = "90vh"
 </script>
 
 <div class="pop-up" on:click={() => $modalState = {"modalType" : null, "modalContent" : null}} transition:fade="{{ duration: 200 }}" >
-	<div class="pop-up-content" on:click|stopPropagation in:fly={{ y: 50 }} out:fly={{ y: 50 }}>
+	<div style="width: {width}; height: {height};" class="pop-up-content" on:click|stopPropagation in:fly={{ y: 50 }} out:fly={{ y: 50 }}>
 		<slot></slot>
 	</div>
 </div>
@@ -26,10 +28,6 @@ div.pop-up {
 	background-color: rgb(0, 0, 0, 0.6);
 
 	div.pop-up-content {
-
-		width: 80vw;
-		height: 90vh;
-
 		background-color: $white;
 	}
 }
