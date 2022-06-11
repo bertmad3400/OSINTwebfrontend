a97c1c67baf33d48cf45d333762ff518a987cde5
bertmad3400 <skrivtilbertram@gmail.com>
2022-06-11 13:23:54 +0200

    Add functions for creating and compiling list of feed names

--- src/lib/user/feeds.js
+++ src/lib/user/feeds.js
@@ -21,3 +21,19 @@ export async function getUserFeeds() {
 		console.log("Failed to get user feeds, with following error: ", currentFeeds.content)
 	}
 }
+
+export async function createFeed(feedSpecs) {
+	console.log(feedSpecs)
+
+	let currentFeeds = await queryProtected("/users/feeds/create", feedSpecs)
+
+	if (currentFeeds.status === "success") {
+		await insertFeeds(currentFeeds.content)
+	} else {
+		console.log(`Failed to create "${feedSpecs.feed_name}" feed, with following error: `, currentFeeds.content)
+	}
+}
+
+export async function getFeedNames(feeds) {
+	return Array.from(Object.values(feeds), (category) => Object.keys(category)).flat()
+}
