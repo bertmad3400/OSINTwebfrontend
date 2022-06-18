3be9f8a66f0f32598381c34ed12be4d32c4dcfbf
bertmad3400 <skrivtilbertram@gmail.com>
2022-06-18 14:26:27 +0200

    Mark article as read, when clicking it

--- src/components/article_lists/list.svelte
+++ src/components/article_lists/list.svelte
@@ -3,12 +3,15 @@
 
 	import { modalState } from "../../shared/stores.js"
 	import { getArticleContent } from "../../lib/articles/main.js"
+	import { modifyCollection } from "../../lib/user/collections.js"
 
 	import ArticleObject from "./articleObject.svelte"
 
 	async function createArticleModal(event) {
 		let articleContent = (await getArticleContent(event.detail.articleID))[0]
 		$modalState = {"modalType" : "article", "modalContent" : articleContent}
+		modifyCollection("Already Read", "extend", event.detail.articleID)
+
 	}
 </script>
 
